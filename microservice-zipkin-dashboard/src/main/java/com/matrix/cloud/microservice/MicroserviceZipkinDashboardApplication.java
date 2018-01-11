444b57a13289691f8c2af8244bf22bf01966e5be
waterif <waterif@yeah.net>
2018-01-11 18:31:18 +0800

    sleuth zipkin

---
+++ microservice-zipkin-dashboard/src/main/java/com/matrix/cloud/microservice/MicroserviceZipkinDashboardApplication.java
@@ -0,0 +1,19 @@
+package com.matrix.cloud.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
+public class MicroserviceZipkinDashboardApplication
+{
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run( MicroserviceZipkinDashboardApplication.class, args );
+    }
+}
