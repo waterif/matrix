78986947ae9de59b025d96dac62558b1b1836589
waterif <waterif@yeah.net>
2018-01-16 18:41:12 +0800

    spring boot admin

--- microservice-admin/src/main/java/com/matrix/cloud/microservice/MicroserviceAdminApplication.java
+++ microservice-admin/src/main/java/com/matrix/cloud/microservice/MicroserviceAdminApplication.java
@@ -1,11 +1,16 @@
 package com.matrix.cloud.microservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
+
+@Configuration
+@EnableAutoConfiguration
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableAdminServer
